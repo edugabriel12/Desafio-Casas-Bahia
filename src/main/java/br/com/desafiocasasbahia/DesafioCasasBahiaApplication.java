4cedabbc74fce7aadbda192e3199808d99f60459
edugabriel12 <gabriellima4539@yahoo.com>
2024-04-23 11:28:18 -0300

    1 - Criação da estrutura do projeto usando o Spring Initializr

---
+++ src/main/java/br/com/desafiocasasbahia/DesafioCasasBahiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiocasasbahia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCasasBahiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioCasasBahiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/desafiocasasbahia/DesafioCasasBahiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiocasasbahia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCasasBahiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
